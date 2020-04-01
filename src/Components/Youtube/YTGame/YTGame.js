53f83848c61d8094418085b20b2d0be602d5811a
Sandra <alecsandra.serediuc@gmail.com>
2020-04-01 13:20:51 +0200

    Get data from the promise and get a random video ID from the api and an array with other 3 song titles for the buttons

--- src/Components/Youtube/YTGame/YTGame.js
+++ src/Components/Youtube/YTGame/YTGame.js
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, {Component}from 'react';
 import YTPlayer from '../YTPlayer/YTPlayer';
 import '../YTGame/YTGame.css';
+import getPlayListItems from '../Api/Api'
+import Shuffle from '../../Utils/Shuffle'
 
-function YTGame() {
+
+  console.log (getPlayListItems())
+
+class YTGame extends Component {
+state ={
+  data: {},
+  oneVideoID:'',
+  randomVideoId:'',
+  playlistItemsArr: [],
+  currentSongTitle:'',
+  fourNonShuffledSongsTitles:[]
+}
+      
+componentDidMount() {
+  // getting from the pending promise the data
+  let promise = getPlayListItems();
+  promise.then((data) => {
+    const modifiedData = getPlayListItems(data);
+    // save the data into the state
+    this.setState ({
+      data: modifiedData,
+  })
+  console.log(data)
+    const arrayPlaylist=[];
+    //Recover the videoIDs from the data(api) and push in an array
+     data.items.map((element)=>{
+      //here be the if statement
+        arrayPlaylist.push(element.snippet.resourceId.videoId)
+      return arrayPlaylist
+    })
+    console.log(arrayPlaylist)
+    // create the random from one videoid
+  const randomVideoId = arrayPlaylist[Math.floor(Math.random()*arrayPlaylist.length)]
+  this.setState({randomVideoId:randomVideoId,
+  // currentSongTitle:
+  })
+  console.log(randomVideoId)
+  // console.log(currentSongTitle)
+
+  //create the array with the title of the songs for the button shuffle(tu put in other buttons)
+   const arraySongTitles=[];
+   data.items.map((element)=>{
+     arraySongTitles.push(element.snippet.title)
+       return arraySongTitles
+     })
+          console.log(arraySongTitles)
+          //shuffle function that reorganize the order of the song title
+const suffledArraySongTitles =  Shuffle(arraySongTitles)
+const fourNonShuffledSongsTitles = suffledArraySongTitles.slice(0, 3); // actually 3
+ console.log(fourNonShuffledSongsTitles)
+// // fourNonShuffledSongsTitles.push(currentSongName); // now 4
+
+//  const fourShuffledSongsTitles = Shuffle(fourNonShuffledSongsTitles)
+// console.log(fourShuffledSongsTitles)
+  // return fourNonShuffledSongsTitles;
+  this.setState({
+    fourNonShuffledSongsTitles:fourNonShuffledSongsTitles
+  })
+})
+ 
+}
+
+
+render(){
   return (
     <div className='yt-all'>
-     <div className='cover-title'>
+    <div className='cover-title'><span className='instuctionsYT'>CLICK ON THE RIGHT ANSWEAR!</span>
     </div>
     <div className="yt-player">
-      <YTPlayer  videoId= 'NFSyl3pwa-A' />
+      <YTPlayer det={this.state.fourNonShuffledSongsTitles} videoId= {this.state.randomVideoId}/> 
+     {/* <Button title={this.state.currentSongTitle} onClick={this.unMute}/> */}
+
     </div>
     </div>
   )
+}
 }
 
 export default YTGame
