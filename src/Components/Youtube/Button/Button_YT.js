a1d4dec44cafa2bd1274ee769bf80cdfb9021798
Sandra <alecsandra.serediuc@gmail.com>
2020-04-01 13:22:50 +0200

    Pass to the button the props

--- src/Components/Youtube/Button/Button_YT.js
+++ src/Components/Youtube/Button/Button_YT.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import '../Button/Button_YT.css';
 
-const ButtonYT= (props) => {
+// const ButtonYT= (props) => {
+
+//     return (
+//         <button className={"yt-button" }>
+//             {props.title}
+//         </button>
+//     )
+// };
+
+// export default ButtonYT;
+const ButtonYT = (props) => {
+    console.log (props)
+    const isCorrect = props.displayedSong === props.currentSong;
 
     return (
-        <button className={"yt-button" }>
-            {props.title}
+        <button className='myButton button' 
+        onClick={props.onClick}>
+        {/* className={"myButton button " + (isCorrect ? "green" : "red")} > */}
+              <span className='titleColor' >{props.displayedSong}</span> 
         </button>
     )
 };
+
 
 export default ButtonYT;
