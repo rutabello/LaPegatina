770f0f285c56a150da2272dc1a7e8997d8a930cd
Sandra <alecsandra.serediuc@gmail.com>
2020-04-16 16:56:59 +0200

    Addd the file with the object(videodata)

---
+++ src/Components/Youtube/VideoDataObject.js
@@ -0,0 +1,115 @@
+const videoDataObject = [
+    {
+      id: 0,
+      title: "Maricarmen",
+      videoId: "lbTkZYQpCWw",
+      VideoURL: "https://www.youtube.com/watch?v=NFSyl3pwa-A",
+      question1:
+      {
+        q: "how much persons are in the band?",
+        ra1: "9",
+        wa1: "3",
+        wa2: "5",
+        wa3:"4"
+      },
+      question2:{
+        q: "where is shoot the video?",
+        ra1: "italy",
+        wa1: "spain",
+        wa2: "Amsterdam",
+        wa3:"usa"
+      },
+      question3:{
+        q: "what colors are used?",
+        ra1: "red",
+        wa1: "green",
+        wa2: "blue",
+        wa3:"grey"
+      }
+    },
+    {   
+        id: 1,
+        title: "Una Mirada",
+        videoId: "_JCGKGgUIuE",
+        VideoURL: "https://www.youtube.com/watch?v=_JCGKGgUIuE",
+        question1:
+        {
+          q: "how much persons are in the band?",
+          ra1: "9",
+          wa1: "3",
+          wa2: "5",
+          wa3:"4"
+        },
+        question2:{
+          q: "where is shoot the video?",
+          ra1: "italy",
+          wa1: "spain",
+          wa2: "Amsterdam",
+          wa3:"usa"
+        },
+        question3:{
+          q: "what colors are used?",
+          ra1: "red",
+          wa1: "green",
+          wa2: "blue",
+          wa3:"grey"
+        }
+      },
+   {
+    id: 2,
+    title: "Olivia",
+    videoId: "kswtbCr3nDQ",
+    VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
+    question1:
+    {
+      q: "how much persons are in the band?",
+      ra1: "9",
+      wa1: "3",
+      wa2: "5",
+      wa3:"4"
+    },
+    question2:{
+      q: "where is shoot the video?",
+      ra1: "italy",
+      wa1: "spain",
+      wa2: "Amsterdam",
+      wa3:"usa"
+    },
+    question3:{
+      q: "what colors are used?",
+      ra1: "red",
+      wa1: "green",
+      wa2: "blue",
+      wa3:"grey"
+    }
+   },
+   {
+    id: 3,
+    title: "Olivia 2",
+    videoId: "kswtbCr3nDQ",
+    VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
+    question1:
+    {
+      q: "how much persons are in the band?",
+      ra1: "9",
+      wa1: "3",
+      wa2: "5",
+      wa3:"4"
+    },
+    question2:{
+      q: "where is shoot the video?",
+      ra1: "italy",
+      wa1: "spain",
+      wa2: "Amsterdam",
+      wa3:"usa"
+    },
+    question3:{
+      q: "what colors are used?",
+      ra1: "red",
+      wa1: "green",
+      wa2: "blue",
+      wa3:"grey"
+    }
+   }     
+]
+export default videoDataObject
