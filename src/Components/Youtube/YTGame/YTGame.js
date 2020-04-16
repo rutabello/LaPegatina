09aac6fdd79ac16a55bb757cdd9e8e51d029d901
Sandra <alecsandra.serediuc@gmail.com>
2020-04-16 17:11:20 +0200

    Changed, so we get now the information from the object, not apicall

--- src/Components/Youtube/YTGame/YTGame.js
+++ src/Components/Youtube/YTGame/YTGame.js
@@ -1,11 +1,10 @@
 import React, {Component}from 'react';
 import YTPlayer from '../YTPlayer/YTPlayer';
 import '../YTGame/YTGame.css';
-import getPlayListItems from '../Api/Api'
-import Shuffle from '../../Utils/Shuffle'
-
-
-  console.log (getPlayListItems())
+// import getPlayListItems from '../Api/Api';
+import Shuffle from '../../Utils/Shuffle';
+import videoDataObject from '../VideoDataObject'
+  // console.log (getPlayListItems())
 
 class YTGame extends Component {
 state ={
@@ -16,50 +15,61 @@ state ={
   currentSongTitle:'',
   fourNonShuffledSongsTitles:[]
 }
-      
-componentDidMount() {
-  // getting from the pending promise the data
-  let promise = getPlayListItems();
-  promise.then((data) => {
-    const modifiedData = getPlayListItems(data);
-    // save the data into the state
-    this.setState ({
-      data: modifiedData,
-  })
-  console.log(data)
-    const arrayPlaylist=[];
-    //Recover the videoIDs from the data(api) and push in an array
-     data.items.map((element)=>{
-      //here be the if statement
-        arrayPlaylist.push(element.snippet.resourceId.videoId)
-      return arrayPlaylist
-    })
-    console.log(arrayPlaylist)
-    // create the random from one videoid
-  const randomVideoId = arrayPlaylist[Math.floor(Math.random()*arrayPlaylist.length)]
-  this.setState({randomVideoId:randomVideoId,
-  // currentSongTitle:
-  })
-  console.log(randomVideoId)
-  // console.log(currentSongTitle)
 
-  //create the array with the title of the songs for the button shuffle(tu put in other buttons)
-   const arraySongTitles=[];
-   data.items.map((element)=>{
-     arraySongTitles.push(element.snippet.title)
-       return arraySongTitles
-     })
-          console.log(arraySongTitles)
-          //shuffle function that reorganize the order of the song title
+componentDidMount=()=> {
+let json = JSON.stringify(videoDataObject);
+let newdata= JSON.parse(json)
+// getting from the pending promise the data
+// let promise = getPlayListItems();
+// promise.then((data) => {
+//   const modifiedData = getPlayListItems(data);
+//   // save the data into the state
+// console.log(json)
+  this.setState ({
+    data: newdata
+})
+console.log(this.state.data)
+console.log (newdata)
+
+
+  const arrayPlaylist=[];
+  //Recover the videoIDs from the data(api) and push in an array
+   newdata.map((element)=>{
+    //here be the if statement
+      arrayPlaylist.push(element.videoId)
+    return arrayPlaylist
+  })
+  console.log(arrayPlaylist)
+
+  // create the random from one videoid
+const randomVideoId = arrayPlaylist[Math.floor(Math.random()*arrayPlaylist.length)]
+this.setState({randomVideoId:randomVideoId,
+// currentSongTitle:
+})
+console.log(randomVideoId)
+// console.log(currentSongTitle)
+
+//create the array with the title of the songs for the button shuffle(tu put in other buttons)
+ const arraySongTitles=[];
+ newdata.map((element)=>{
+   arraySongTitles.push(element.title)
+     return arraySongTitles
+   })
+        console.log(arraySongTitles)
+        //shuffle function that reorganize the order of the song title
 const suffledArraySongTitles =  Shuffle(arraySongTitles)
 const fourNonShuffledSongsTitles = suffledArraySongTitles.slice(0, 3); // actually 3
- console.log(fourNonShuffledSongsTitles)
+console.log(fourNonShuffledSongsTitles)
+// fourNonShuffledSongsTitles.push(currentSongName); // now 4
 
-  this.setState({
-    fourNonShuffledSongsTitles:fourNonShuffledSongsTitles
-  })
+//  const fourShuffledSongsTitles = Shuffle(fourNonShuffledSongsTitles)
+// console.log(fourShuffledSongsTitles)
+// return fourNonShuffledSongsTitles;
+this.setState({
+  fourNonShuffledSongsTitles:fourNonShuffledSongsTitles
 })
- 
+// })
+
 }
 
 
