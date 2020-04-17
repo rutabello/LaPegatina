3dfe67127f4d09d3c2f5b005df8c013a77eb4cfb
Sandra <alecsandra.serediuc@gmail.com>
2020-04-17 20:17:07 +0200

    Addapted the object to who we need to use

--- src/Components/Youtube/VideoDataObject.js
+++ src/Components/Youtube/VideoDataObject.js
@@ -1,115 +1,88 @@
+// always the first question is the right one
 const videoDataObject = [
     {
       id: 0,
       title: "Maricarmen",
       videoId: "lbTkZYQpCWw",
       VideoURL: "https://www.youtube.com/watch?v=NFSyl3pwa-A",
-      question1:
+      questions:[
       {
-        q: "how much persons are in the band?",
-        ra1: "9",
-        wa1: "3",
-        wa2: "5",
-        wa3:"4"
+        question: "how much persons are in the band?",
+        answers:[9,3,5,4]
       },
-      question2:{
-        q: "where is shoot the video?",
-        ra1: "italy",
-        wa1: "spain",
-        wa2: "Amsterdam",
-        wa3:"usa"
+      {
+        question: "where is shoot the video?",
+        answers: ["italy","spain","Amsterdam","usa"]
+       
       },
-      question3:{
-        q: "what colors are used?",
-        ra1: "red",
-        wa1: "green",
-        wa2: "blue",
-        wa3:"grey"
+      {
+        question: "what colors are used?",
+        answers: ["red","green", "blue","grey"]
       }
-    },
+    ]
+},
     {   
         id: 1,
         title: "Una Mirada",
         videoId: "_JCGKGgUIuE",
         VideoURL: "https://www.youtube.com/watch?v=_JCGKGgUIuE",
-        question1:
-        {
-          q: "how much persons are in the band?",
-          ra1: "9",
-          wa1: "3",
-          wa2: "5",
-          wa3:"4"
-        },
-        question2:{
-          q: "where is shoot the video?",
-          ra1: "italy",
-          wa1: "spain",
-          wa2: "Amsterdam",
-          wa3:"usa"
-        },
-        question3:{
-          q: "what colors are used?",
-          ra1: "red",
-          wa1: "green",
-          wa2: "blue",
-          wa3:"grey"
-        }
-      },
+        questions:[
+            {
+                question: "how much persons are in the band?",
+                answers:[9,3,5,4]
+              },
+              {
+                question: "where is shoot the video?",
+                answers: ["italy","spain","Amsterdam","usa"]
+               
+              },
+              {
+                question: "what colors are used?",
+                answers: ["red","green", "blue","grey"]
+              }
+    ]
+},
    {
     id: 2,
     title: "Olivia",
     videoId: "kswtbCr3nDQ",
     VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
-    question1:
-    {
-      q: "how much persons are in the band?",
-      ra1: "9",
-      wa1: "3",
-      wa2: "5",
-      wa3:"4"
-    },
-    question2:{
-      q: "where is shoot the video?",
-      ra1: "italy",
-      wa1: "spain",
-      wa2: "Amsterdam",
-      wa3:"usa"
-    },
-    question3:{
-      q: "what colors are used?",
-      ra1: "red",
-      wa1: "green",
-      wa2: "blue",
-      wa3:"grey"
-    }
+    questions:[
+        {
+            question: "how much persons are in the band?",
+            answers:[9,3,5,4]
+          },
+          {
+            question: "where is shoot the video?",
+            answers: ["italy","spain","Amsterdam","usa"]
+           
+          },
+          {
+            question: "what colors are used?",
+            answers: ["red","green", "blue","grey"]
+          }
+]
    },
    {
     id: 3,
     title: "Olivia 2",
     videoId: "kswtbCr3nDQ",
     VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
-    question1:
-    {
-      q: "how much persons are in the band?",
-      ra1: "9",
-      wa1: "3",
-      wa2: "5",
-      wa3:"4"
-    },
-    question2:{
-      q: "where is shoot the video?",
-      ra1: "italy",
-      wa1: "spain",
-      wa2: "Amsterdam",
-      wa3:"usa"
-    },
-    question3:{
-      q: "what colors are used?",
-      ra1: "red",
-      wa1: "green",
-      wa2: "blue",
-      wa3:"grey"
-    }
+    questions:[
+        {
+            question: "how much persons are in the band?",
+            answers:[9,3,5,4]
+          },
+          {
+            question: "where is shoot the video?",
+            answers: ["italy","spain","Amsterdam","usa"]
+           
+          },
+          {
+            question: "what colors are used?",
+            answers: ["red","green", "blue","grey"]
+          }
+]
    }     
 ]
 export default videoDataObject
