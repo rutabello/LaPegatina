712e905ab95a4c2b05b25fd7870ab26bbcdba081
Sandra <alecsandra.serediuc@gmail.com>
2020-04-21 19:08:44 +0200

    Change the object with 4 videos and questions recieved from the band

--- src/Components/Youtube/VideoDataObject.js
+++ src/Components/Youtube/VideoDataObject.js
@@ -2,85 +2,121 @@
 const videoDataObject = [
     {
       id: 0,
-      title: "Maricarmen",
-      videoId: "lbTkZYQpCWw",
-      VideoURL: "https://www.youtube.com/watch?v=NFSyl3pwa-A",
+      title: "Gat Rumberu",
+      videoId: "XYKeFkKmSt4",
+      VideoURL: " https://www.youtube.com/watch?v=XYKeFkKmSt4",
       questions:[
       {
-        question: "how much persons are in the band?",
-        answers:[9,3,5,4]
+        question: "Quién compuso la canción? ",
+        answers:["Ferran","Adrià","Rubén","Romain"]
       },
       {
-        question: "where is shoot the video?",
-        answers: ["italy","spain","Amsterdam","usa"]
-       
+        question: "En qué ciudades está grabado? ",
+        answers: ["Bruselas y Mataró","Ámsterdam y Barcelona","Rennes y Barcelona",
+          "Barcelona y Mataró"]
       },
       {
-        question: "what colors are used?",
-        answers: ["red","green", "blue","grey"]
-      }
+        question: "Cuáles son los dos equipos ‘raperos’ que hay? ",
+        answers: ["Ferran, Romain y Rubén VS. Adrià, Axel y Ovidi","Ferran, Romain y Sergi VS. Adrià, Ovidi y Rubén", 
+        "Adrià, Romain, Rubén VS. Axel, Ovidi y Sergi","Miki, Romain y Rubén VS. Adrià, Axel y Sergi"]
+      },
+      {
+        question: "Qué técnico de La Pegatina sale en los créditos?",
+        answers: ["Tamu (backliner)","Chema (merchandising)","Jorge (técnico de luces)",
+        "Abel (técnico de sonido"]
+      },
+      {
+        question: "Qué disco contiene “Gat Rumberu”?",
+        answers: ["Via Mandarina","Al Carrer!","Xapomelon",
+        " Ahora o Nunca"]
+      },
     ]
 },
     {   
         id: 1,
-        title: "Una Mirada",
-        videoId: "_JCGKGgUIuE",
-        VideoURL: "https://www.youtube.com/watch?v=_JCGKGgUIuE",
+        title: "La Tempestad ",
+        videoId: "9aEVNajprpg",
+        VideoURL: " https://www.youtube.com/watch?v=9aEVNajprpg",
         questions:[
             {
-                question: "how much persons are in the band?",
-                answers:[9,3,5,4]
+                question: "Qué cantante colabora en esta canción?",
+                answers:["Eva Amaral","Ms. Maiko","Rozalén","La Canija"]
               },
               {
-                question: "where is shoot the video?",
-                answers: ["italy","spain","Amsterdam","usa"]
+                question: "Quién empieza la guerra de comida con un descuido?",
+                answers: ["Romain","Eva Amaral","Miki","Ferran"]
                
               },
               {
-                question: "what colors are used?",
-                answers: ["red","green", "blue","grey"]
+                question: "De qué color es el paraguas de Eva Amaral?",
+                answers: ["Transparente","Rojo", "Violeta","Amarillo"]
+              },
+              {
+                question: "Qué elemento explota al final de la canción?",
+                answers: ["Pastel","Botella", "Naranja","Vaso"]
+              },
+              {
+                question: "Qué golosina le tiran a Miki en la cara?",
+                answers: ["Nubes","Chupa Chups", "Bragas","Ositos"]
               }
     ]
 },
    {
     id: 2,
-    title: "Olivia",
-    videoId: "kswtbCr3nDQ",
-    VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
+    title: "Heridas de Guerra",
+    videoId: "0BBfw3T8Mus",
+    VideoURL: "https://www.youtube.com/watch?v=0BBfw3T8Mus",
     questions:[
         {
-            question: "how much persons are in the band?",
-            answers:[9,3,5,4]
+            question: "En qué barrio de Barcelona está situada la terraza del videoclip?",
+            answers:["Gràcia","Poble Nou","Sants","Nou Barris"]
           },
           {
-            question: "where is shoot the video?",
-            answers: ["italy","spain","Amsterdam","usa"]
+            question: "Qué instrumento toca Romain que nunca toca en un escenario?",
+            answers: ["Guitarra eléctrica","Percusión","Teclado","Contrabajo"]
            
           },
           {
-            question: "what colors are used?",
-            answers: ["red","green", "blue","grey"]
+            question: "Qué elemento tiramos al principio del videoclip y nunca falta en nuestros conciertos?",
+            answers: ["Confeti","Camiseta", "Humo","Agua"]
+          },
+          {
+            question: "“Heridas de Guerra” fue single de qué disco?",
+            answers: ["Revulsiu","Un secreto a voces", "Eureka!","Al Carrer!"]
+          },
+          {
+            question: "Nos encanta grabar escenas de agua en invierno. Cómo grabamos en la piscina?",
+            answers: ["El agua estaba caliente y había una ventana debajo del agua","El agua estaba caliente", 
+            "Había una ventana debajo del agua","El director es un crack de la Gopro"]
           }
 ]
    },
    {
     id: 3,
-    title: "Olivia 2",
-    videoId: "kswtbCr3nDQ",
-    VideoURL: "https://www.youtube.com/watch?v=kswtbCr3nDQ",
+    title: "Lloverá y yo veré ",
+    videoId: "EoD4zkM24tU",
+    VideoURL: "- https://www.youtube.com/watch?v=EoD4zkM24tU",
     questions:[
         {
-            question: "how much persons are in the band?",
-            answers:[9,3,5,4]
+            question: "Qué palabra utilizarías para definir esta canción?",
+            answers:["Adrenalina","Aburrida","Oscura","Soporífera"]
           },
           {
-            question: "where is shoot the video?",
-            answers: ["italy","spain","Amsterdam","usa"]
+            question: "De qué país asiático son las imágenes que salen en el vídeo?",
+            answers: ["China","Vietnam","Indonesia","Filipinas"]
            
           },
           {
-            question: "what colors are used?",
-            answers: ["red","green", "blue","grey"]
+            question: "En qué año salió esta canción?",
+            answers: [ 2013,2009, 2015,2018]
+          },
+          {
+            question: "Quién toca la guitarra española en esta canción?",
+            answers: ["Rubén","Ferran", "Sergi","Adrià"]
+          },
+          {
+            question: "Cómo bailarías en esta canción?",
+            answers: ["Haciendo pogo","A lo paso doble", "A lo salsero","Tipo vals"]
           }
 ]
    }     
