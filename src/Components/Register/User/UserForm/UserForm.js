3969fdbfcd5197350d91852052a98aca121645da
Sandra <alecsandra.serediuc@gmail.com>
2020-04-22 22:01:17 +0200

    Change the component as a modal and changed the form components

--- src/Components/Register/User/UserForm/UserForm.js
+++ src/Components/Register/User/UserForm/UserForm.js
@@ -1,107 +1,128 @@
 import React, { Component } from "react";
+import {MyContext} from '../../../../context/MyProvider'
+import SignUp from './Signup';
+import Login from './Login'
+import Panel from './Panel'
+import FormPanel from './FormPanel';
+import MyProvider from '../../../../context/MyProvider'
 
 class UserForm extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-        lastname: '',
-        firstname: '',
-        email: '',
-        notLogged: true,
-      };
+  //   this.state = {
+  //       lastname: '',
+  //       firstname: '',
+  //       email: '',
+  //       notLogged: true,
+  //     };
 
-    this.onChange = this.onChange.bind(this);
-    this.onClick = this.onClick.bind(this);
-    this.submitForm = this.submitForm.bind(this);
-    this.apiURL = this.apiURL.bind(this);
-  }
-  apiURL() {
-    const url = "";
-    const config = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(this.state)
-    };
+  //   this.onChange = this.onChange.bind(this);
+  //   this.onClick = this.onClick.bind(this);
+  //   this.submitForm = this.submitForm.bind(this);
+  //   this.apiURL = this.apiURL.bind(this);
+  // }
+  // apiURL() {
+  //   const url = "";
+  //   const config = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(this.state)
+  //   };
 
-    fetch(url, config)
-      .then(res => res.json())
-      .then(res => {
-        if (res.error) {
-          alert(res.error);
-        } else {
-          alert(`Benvingut! Los puntos son guardados!`);
-        }
-      })
-      .catch(e => {
-        console.error(e);
-        alert("Prova otra vez");
-      });
-  }
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  onClick(e) {
-    this.apiURL();
-  }
-  submitForm(e) {
-    e.preventDefault();
-    this.setState({
-      notLogged:false
-    })
-  }
+  //   fetch(url, config)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       if (res.error) {
+  //         alert(res.error);
+  //       } else {
+  //         alert(`Benvingut! Los puntos son guardados!`);
+  //       }
+  //     })
+  //     .catch(e => {
+  //       console.error(e);
+  //       alert("Prova otra vez");
+  //     });
+  // }
+  // onChange(e) {
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // }
+  // onClick(e) {
+  //   this.apiURL();
+  // }
+  // submitForm(e) {
+  //   e.preventDefault();
+  //   this.setState({
+  //     notLogged:false
+  //   })
+  // }
   render() {
     return (
-      <div className="FormUser">
-        <h1>User form</h1>
+      <div>
+      <button type="button" className="btn btn-primary" 
+      data-toggle="modal" data-target="#sharethissong">
+          Register
+      </button>
+            <div
+              className="modal fade"
+                  id="sharethissong"
+                  id="sharethissong"
+                  tabIndex="-1"
+                  role="dialog"
+                  aria-labelledby="exampleModalCenterTitle"
+                  aria-hidden="true"
+                  >
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                  <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="sharesongs">Welcome, keep playing!</h5>
+                          <button type="button" className="close" 
+                          data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                           </button>
+                        </div>
+<div className="modal-body"></div>
+<div className="form-wrap">
+      <MyProvider>
+        <div className="tabs">
+          <Panel id="login">
+            <h2 className="login-tab">Login</h2>
+          </Panel>
+          <Panel id="signup">
+            <h2 className="signup-tab">Sign Up</h2>
+          </Panel>
+        </div>
 
-        <form onSubmit={this.submitForm}>
-    <fieldset>
-      <legend>Information</legend>
-      <div className="form-data">
-        <label htmlFor="lastname">Last Name</label>
-        <input
-          type="text"
-          id="lastname"
-          name="lastname"
-          onChange={this.onChange}
-          value={this.state.lastname}
-        />
-      </div>
+        <FormPanel isActive="login">
+          <Login />
+        </FormPanel>
 
-      <div className="form-data">
-        <label htmlFor="firstname">First Name</label>
-        <input
-          type="text"
-          id="firstname"
-          name="firstname"
-          onChange={this.onChange}
-          value={this.state.firstname}
-        />
-      </div>
-
-      <div className="form-data">
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={this.onChange}
-          value={this.state.email}
-        />
-      </div>
-      <hr />
-      <div className="form-data">
-        <input type="submit" value="Send" />
-      </div>
-    </fieldset>
-  </form>
-</div>
-    );
+        <FormPanel isActive="signup">
+          <SignUp />
+        </FormPanel>
+      </MyProvider>
+    </div>
+  );
+};
+<div className="modal-footer">
+                                                        <button type="button" className="btn btn-primary" data-dismiss="modal">
+                                                            cerar
+                                                        </button>
+                                                        </div>
+                                                        </div>
+                                                        </div>
+                                                        </div>
+                                                        </div>                                  
+                                                    
+    )
   }
 }
 export default UserForm;
+
+
+                                      
+                                                    
