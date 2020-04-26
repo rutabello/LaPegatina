08b942390870e6b67ca8e61026ea90a6d458a0ed
Sandra <alecsandra.serediuc@gmail.com>
2020-04-26 15:58:26 +0200

    Added more videos with questions to the videodata object

--- src/Components/Youtube/VideoDataObject.js
+++ src/Components/Youtube/VideoDataObject.js
@@ -95,7 +95,7 @@ const videoDataObject = [
         id: 3,
         title: 'Lloverá y yo veré ',
         videoId: 'EoD4zkM24tU',
-        VideoURL: '- https://www.youtube.com/watch?v=EoD4zkM24tU',
+        VideoURL: 'https://www.youtube.com/watch?v=EoD4zkM24tU',
         questions: [
             {
                 question: 'Qué palabra utilizarías para definir esta canción?',
@@ -120,5 +120,238 @@ const videoDataObject = [
             },
         ],
     },
+    {
+        id: 4,
+        title: 'Olivia (Mario Diaz)',
+        videoId: 'kswtbCr3nDQ',
+        VideoURL: 'https://www.youtube.com/watch?v=kswtbCr3nDQ',
+        questions: [
+            {
+                question: 'Qué animal acompaña a La Pegatina en este videoclip?',
+                answers: ['Un oso', 'Un ciervo', 'Un pingüino', 'Un lagarto'],
+            },
+            {
+                question: 'Quién colabora en esta canción?',
+                answers: ['Mario Díaz', 'Amaral', 'La gran Pegatina', 'Rayden'],
+            },
+            {
+                question: ' Dónde se grabó este videoclip?',
+                answers: ['Madrid', 'Granada', 'Bilbao', 'Santiago de Compostela'],
+            },
+            {
+                question: 'De qué color es la cresta punky de Romain en este video?',
+                answers: ['Granate', 'Amarillo', 'Plata', 'Verde'],
+            },
+            {
+                question: 'A qué estilo musical pertenece esta canción?',
+                answers: ['Rumba', 'Rock', ' Merengue', 'Cumbia'],
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: 'Non é facilé',
+        videoId: 'vM0gUacrp4g',
+        VideoURL: 'https://www.youtube.com/watch?v=vM0gUacrp4g',
+        questions: [
+            {
+                question: 'Qué idioma se utilizó para dar el nombre a esta canción?',
+                answers: ['Italiano', 'Català', 'Francés', 'Portugués'],
+            },
+            {
+                question: 'Quién colabora en esta canción?',
+                answers: ['Baciamolemani', 'Manu Chao', 'Macaco', 'Ska-P'],
+            },
+            {
+                question: 'Con qué objeto canta Adrià en esta canción?',
+                answers: ['Una bombilla', 'Un arbusto', 'Un palo de escoba', 'Un bocadillo'],
+            },
+            {
+                question: 'Esta canción salió para uno de los aniversarios del grupo. Cuál?',
+                answers: ['El décimo aniversario', 'El quince aniversario', 'El segundo aniversario', 'El quinto aniversario'],
+            },
+            {
+                question: 'Cuando salió esta canción, con cuántos miembros oficiales contaba La Pegatina?',
+                answers: ['7', '4', '5', '6'],
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: 'Miranda',
+        videoId: 'zyiNZqKumGA',
+        VideoURL: 'https://www.youtube.com/watch?v=zyiNZqKumGA',
+        questions: [
+            {
+                question: 'Cómo se llama la gira de 2010 a la que dio nombre este videoclip?',
+                answers: ['Mira que Mira Tour', 'Miranda Tour', 'Via Mandarina Tour', 'Lloverá y Yo veré tour'],
+            },
+            {
+                question: 'Qué tenemos en común todos los presentes en el video?',
+                answers: ['Barba', 'La misma camisa', 'El mismo peinado', 'La misma camisa y peinado'],
+
+            },
+            {
+                question: 'Qué tipo de vehículo es el que sale en el vídeo?',
+                answers: ['Furgoneta', 'Camión', 'Caravana', 'Coche'],
+            },
+            {
+                question: 'En qué localización está grabado?',
+                answers: ['Túnel de lavado', 'Parque', 'Carretera', 'Parking'],
+            },
+            {
+                question: 'Qué instrumento utiliza Romain en el video?',
+                answers: ['Melódica', 'Flauta', 'Mini Piano', 'Acordeón'],
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: 'Penjat',
+        videoId: 'HYmWxl5MSg0',
+        VideoURL: 'https://www.youtube.com/watch?v=HYmWxl5MSg0',
+        questions: [
+            {
+                question: 'Qué grupo colabora en esta canción?',
+                answers: ['Txarango', 'Doctor Prats', 'Che Sudaka', 'Bongo Botrako'],
+            },
+            {
+                question: 'En qué lugar está grabado el videoclip?',
+                answers: ['Barcelona', 'Cerdanyola del Vallès', 'Badalona', 'Montcada i Reixac'],
+
+            },
+            {
+                question: 'Qué instrumento toca Ovidi en el vídeo?',
+                answers: ['El bombo y el cajón flamenco', 'Bombo', 'Cajón flamenco', ' Pandereta '],
+            },
+            {
+                question: 'Cuántos componentes de La Pegatina hay en el vídeo?',
+                answers: ['4', '8', '7', '5'],
+            },
+            {
+                question: 'De qué estilo de música forma parte esta canción?',
+                answers: ['Rumba', 'Salsa', 'Ska', 'Bulería'],
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: 'Aquí es nadal i Estic Content',
+        videoId: '3LSA4xXWcUM',
+        VideoURL: 'https://www.youtube.com/watch?v=3LSA4xXWcUM',
+        questions: [
+            {
+                question: 'Qué matrícula tiene el vehículo que sale al inicio del vídeo?',
+                answers: ['La Pegatina', 'No tiene matricula', '7270HPC', '9999LPG'],
+            },
+            {
+                question: 'Con qué bebida brindamos en el vídeo?',
+                answers: ['Cava', 'Vino', 'Agua', 'Ron'],
+
+            },
+            {
+                question: 'En qué campaña de Navidad salió esta canción?',
+                answers: ['TV3-Catalunya Ràdio 2010', 'Tve 2012', 'Btv 2011', 'Ninguna de las anteriores'],
+            },
+            {
+                question: 'Quién aparece al final del video para echar al resto de La Pegatina del salón?',
+                answers: ['Tamu', 'Un Actor', 'Miki', 'No Aparece nadie al final'],
+            },
+            {
+                question: 'Qué animal aparece con Ovidi al inicio del vídeo?',
+                answers: ['Un burro', 'Un perro', 'Un gato', 'Una serpiente'],
+            },
+        ],
+    },
+    {
+        id: 9,
+        title: 'La Sorranchera',
+        videoId: 'UY7CSscOxHQ',
+        VideoURL: 'https://www.youtube.com/watch?v=UY7CSscOxHQ',
+        questions: [
+            {
+                question: 'Dónde se grabó el videoclip?',
+                answers: ['Cabo de Gata', 'Cabo Vidio', 'Cabo de Palos', 'Cabo Cañaveral'],
+            },
+            {
+                question: 'Quién se desmayó durante el rodaje?',
+                answers: ['Rubén', 'Adrià', 'Ferran', 'Sergi'],
+
+            },
+            {
+                question: 'A quién le cayó la GoPro al fondo del mar al lanzarse al agua desde 8m de altura?',
+                answers: ['Rubén', 'Ferran', 'Ovidi', 'Miki'],
+            },
+            {
+                question: 'Qué otro videoclip se grabó en la provincia de Almería?',
+                answers: ['Maricarmen', 'Muérdeme', 'Foxy & Billy', 'Olivia'],
+            },
+            {
+                question: 'Rodamos parte de este vídeo en El Cortijo del Fraile, donde. en 1928, fue cometido un crimen muy conocido que inspiró una obra de Lorca. Cuál?',
+                answers: ['Bodas de Sangre', 'La casa de Bernarda Alba', 'Doña Rosita Soltera', 'Ninguna de las anteriores'],
+            },
+        ],
+    },
+    {
+        id: 10,
+        title: 'Una mirada (Ska-P)',
+        videoId: '_JCGKGgUIuE',
+        VideoURL: 'https://www.youtube.com/watch?v=_JCGKGgUIuE',
+        questions: [
+            {
+                question: 'En qué programa de TV se inspira el videoclip?',
+                answers: ['Takeshi’s Castle (Humor Amarillo)', 'Crónicas Marcianas', 'Wrestling Society X', 'Glow'],
+            },
+            {
+                question: 'Dónde grabó Pulpul su aparición?',
+                answers: ['En el campo de al lado de su casa', 'Con el resto de la banda', 'En su casa', 'En el patio de su casa'],
+
+            },
+            {
+                question: 'Qué políticos disfrazados aparecen?',
+                answers: ['Todas las anteriores', 'Obama', 'Putin', 'Merkel y Rajoy'],
+            },
+            {
+                question: 'Qué animal tiene las manos Rajoy?',
+                answers: ['Cangrejo', 'Pulpo', 'Perro', 'Tigre'],
+            },
+            {
+                question: 'Qué animal aparece con Ovidi al inicio del vídeo?',
+                answers: ['Un burro', 'Un perro', 'Un gato', 'Una serpiente'],
+            },
+        ],
+    },
+    {
+        id: 11,
+        title: 'Alosque (DCallaos)',
+        videoId: 'XadfwGssSaU',
+        VideoURL: 'https://www.youtube.com/watch?v=XadfwGssSaU',
+        questions: [
+            {
+                question: 'Dónde fue grabada la parte del baile del videoclip?',
+                answers: ['En Montcada i Reixac', 'En Sant Just Desvern', 'En Sant Joan Despí', 'En Barcelona'],
+            },
+            {
+                question: 'El videoclip de “Alosque” fue:',
+                answers: ['Ninguna de las anteriores es correcta', 'El primer videoclip del grupo', 'El único videoclip grabado en nuestro pueblo', 'El único en que sale Maribel La Canija'],
+
+            },
+            {
+                question: 'La canción Alosque fue estrenada en:',
+                answers: ['Sala Apolo 2008', 'Disco “Via Mandarina”', 'Disco “Al Carrer!”', 'Sala Taboo 2008'],
+            },
+            {
+                question: 'Cómo acaba el videoclip?',
+                answers: ['Concierto improvisado acústico', 'Con el niño del cometa del inicio', 'Con Romain rompiendo el acordeón contra el suelo', 'Cada uno pa’ su casa'],
+            },
+            {
+                question: 'Dónde estaba Miki Florensa cuando estaban grabando este videoclip?',
+                answers: ['Las anteriores son correctas', 'Tocando el saxo', 'Empezando a tocar la guitarra', ' Empezando a tocar con MondoLoco'],
+            },
+        ],
+    },
+
+
 ];
+
 export default videoDataObject;
