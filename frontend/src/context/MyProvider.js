fcd9cf4b3634ced7a5594a9f9a56cc048f884801
Sandra <alecsandra.serediuc@gmail.com>
2020-05-18 21:57:04 +0200

    last myprovider version

--- frontend/src/context/MyProvider.js
+++ frontend/src/context/MyProvider.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 export const MyContext = React.createContext();
 
+
 class MyProvider extends Component {
 
     state = {
@@ -24,9 +25,30 @@ class MyProvider extends Component {
         authed: true,
     }
 
+    updateTotalPoints = () => {
+        const keysArray = [
+            'points_spotify_round_one',
+            'points_spotify_round_two',
+            'points_instagram_round_one',
+            'points_instagram_round_two',
+            'points_youtube_round_one',
+            'points_youtube_round_two',
+        ];
+
+        let total = 0;
+
+        keysArray.forEach((key) => {
+            total += this.state[key];
+        });
+
+        this.setState({
+            total_app_points: total,
+        });
+    };
+
     render() {
 
-        const { points: total_app_points, activePanel } = this.state;
+        const { points: activePanel } = this.state;
         // We rename points so we can after do 'points: this.state.points + points' without problems
 
         const { children } = this.props;
@@ -34,9 +56,14 @@ class MyProvider extends Component {
         return (
             <MyContext.Provider value={{
                 state: this.state,
-                addPoints: (points) => this.setState({
-                    points: total_app_points + points,
-                }),
+
+                addPoints: (newPoints, gameName, roundIn) => {
+                    if (newPoints >= this.state[`points_${gameName}_round_${roundIn}`]) {
+                        this.setState({
+                            [`points_${gameName}_round_${roundIn}`]: newPoints,
+                        }, this.updateTotalPoints);
+                    }
+                },
 
                 clearUser: () => this.setState({
                     first_name: '',
@@ -62,11 +89,18 @@ class MyProvider extends Component {
                     username: data[0].username,
                     birth_date: data[0].birth_date,
                     email: data[0].email,
+                    points_spotify_round_one: data[0].points_spotify_round_one,
+                    points_spotify_round_two: data[0].points_spotify_round_two,
+                    points_instagram_round_one: data[0].points_instagram_round_one,
+                    points_instagram_round_two: data[0].points_instagram_round_two,
+                    points_youtube_round_one: data[0].points_youtube_round_one,
+                    points_youtube_round_two: data[0].points_youtube_round_two,
                     activePanel: 'login',
                     language: 'spanish',
                     authed: true,
                 }),
 
+                replaceState: (newState) => this.setState(newState),
 
                 // login sign up switch
                 activePanel,
