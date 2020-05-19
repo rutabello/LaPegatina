fff97f333a4158cc81ece5e71380b3b1de9649b3
Rut <rut.abello@gmail.com>
2020-05-19 13:17:03 +0200

    Prueba gitignore

--- backend/conf.js
+++ backend/conf.js
@@ -1,9 +1,21 @@
+// const  mysql = require('mysql');
+// const  connection = mysql.createConnection({
+//   host :  'localhost', // address of the server
+//   user :  'root', // username
+//   password :  'password',
+//   database :  'la_pegatina',
+// });
+
+// module.exports = connection;
+
+
+
 const  mysql = require('mysql');
 const  connection = mysql.createConnection({
-  host :  'localhost', // address of the server
-  user :  'root', // username
-  password :  'password',
-  database :  'la_pegatina',
+  host :  'eu-cdbr-west-03.cleardb.net', // address of the server
+  user :  'b5f9b9d64a08a1', // username
+  password :  'b92c0705',
+  database :  'heroku_1c259aae7108d91',
 });
 
 module.exports = connection;
